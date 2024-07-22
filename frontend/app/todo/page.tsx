36f63201ebdacd6c0d40534d46bc2393910aacc1
takada-yuto <yuto1999.0516@gmail.com>
2024-07-22 23:46:34 +0900

    リファクタリング

--- frontend/app/todo/page.tsx
+++ frontend/app/todo/page.tsx
@@ -4,7 +4,7 @@ const Todo = () => {
   return (
     <header className="bg-gradient-to-r from-ivory-500 to-ivory-500 shadow-md mb-4 p-4">
       <h1 className={`text-3xl text-center ${RampartOneFont.className}`}>
-        Todo Appppppppp
+        Todo App
       </h1>
     </header>
   );
