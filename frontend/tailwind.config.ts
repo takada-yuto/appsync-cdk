24d3598f200f6f9e21d4587525dc4939f7ee0acb
takada-yuto <yuto1999.0516@gmail.com>
2024-07-22 23:47:09 +0900

    コンフリクト解消漏れ

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,36 +1,28 @@
-import type { Config } from "tailwindcss"
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        ivory: "#F5F5DC",
-        lightblue: "#B0C4DE",
-        lightgray: "#778899",
+        ivory: '#F5F5DC',
+        lightblue: '#B0C4DE',
+        lightgray: '#778899',
       },
       fontFamily: {
-        Raleway: ["Raleway", "sans-serif"],
-      },
-      colors: {
-        ivory: "#F5F5DC",
-        lightblue: "#B0C4DE",
-        lightgray: "#778899",
-      },
-      fontFamily: {
-        Raleway: ["Raleway", "sans-serif"],
+        Raleway: ['Raleway', 'sans-serif'],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
